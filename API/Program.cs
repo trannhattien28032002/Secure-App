f6cc915789ffe51157c30afaca1329fc2f3dadd4
Tyler Tran <trannhattien28032002@gmail.com>
2025-03-15 22:06:57 +0700

    Add JWT Authentication

--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,62 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+var JWTSetting = builder.Configuration.GetSection("");
+
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options=>options.UseSqlite("Data Source-auth.db"));
+
+builder.Services.AddIdentity<AppUser.IdentityRole>()
+.AddEntityFrameworkStores<AppDbContext>()
+.AddDefaultTokenProviders();
+
+builder.Services.AddAuthentication(opt=>{
+    opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    opt.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(opt=>{
+    opt.SaveToken = true;
+    opt.RequireHttpsMetadata = false;
+    opt.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidAudience = JWTSetting["ValidAudience"],
+        ValidIssuer = JWTSetting["ValidIssuer"],
+        IssuerSigningKey = new SymmericSecurityKey(Encoding.UTF8.GetBytes(JWTSetting.GetSection("securityKey").Value!));
+    }
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => {
+    c.AddSecurityDefinition("Bearer", new OpenAPISecurityScheme{
+        Description = @"JWT Authorization Example : 'Bearer eyeleieieekeieieie",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement() {
+        new OpenApiSecurityScheme({
+            Reference = new OpenApiReference
+            {
+                Type = ReferenceType.SecurityScheme,
+                Id = "Bearer"
+            },
+            Scheme = "outh2",
+            Name = "Bearer",
+            In = ParameterLocation.Header,
+        }),
+        new List<string>();
+    })
+});
 
 var app = builder.Build();
 
@@ -17,6 +68,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseAuthentication();
 
 app.UseAuthorization();
 
