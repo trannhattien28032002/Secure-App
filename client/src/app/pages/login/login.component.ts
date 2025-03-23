4efeeb0a2de2616805d713720b1c690345cdc023
Tyler Tran <trannhattien28032002@gmail.com>
2025-03-23 23:54:23 +0700

    Design Login Form (50%)

--- client/src/app/pages/login/login.component.ts
+++ client/src/app/pages/login/login.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [MatInputModule, MatIconModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
